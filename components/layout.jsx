73058b6704790aaab0a3cb6d0f187cdff57e7fa2
jackytank <balisongian@gmail.com>
2022-06-25 20:38:52 +0700

    .

--- components/layout.jsx
+++ components/layout.jsx
@@ -5,7 +5,7 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Jackytank";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "Jackytank Blog";
 
 export default function Layout({ children, home }) {
 	return (
